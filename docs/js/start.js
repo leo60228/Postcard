7b39f596d3c52805434be497a36ce5cfa5892749
leo60228 <iakornfeld@gmail.com>
2019-01-07 14:13:34 -0500

    fix even dumber error

--- docs/js/start.js
+++ docs/js/start.js
@@ -36,7 +36,7 @@ document.getElementById('form').addEventListener('submit', async e => {
     let readName = data.children.filter(e => e.name == 'meta')[0].attributes.Name;
     if (readName !== undefined) {
       console.log(`[Postcard] Read map name: ${bin}`);
-    }
+    } else throw 'No map name exists in metadata (this is not an error)';
   } catch (ex) {
     console.warn(`[Postcard] Error locating map name: ${ex}`);
     bin = name();

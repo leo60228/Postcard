25061238d6e26a3188c8917d6765796f990caf9e
leo60228 <iakornfeld@gmail.com>
2019-01-07 13:58:32 -0500

    Add proper API

--- docs/api.jl
+++ docs/api.jl
@@ -1,25 +1,12 @@
 using Base64
-import HTTP.URIs: escapeuri
+
+function compute_api_url(data, name)
+    base64 = base64encode(data)
+    startpath = "https://digitalocean.leo60228.space/testing/start/"
+    return "$startpath#$(basename(name)):$base64"
+end
 
 path = ARGS[1]
 data = read(path)
-base64 = base64encode(data)
 
-uri = "https://digitalocean.leo60228.space/testing/api.html"
-
-html =  """
-        <iframe src="$(uri)"></iframe>
-        <script>
-            b='$(base64)';
-            onmessage=e=>eval(e.data)
-        </script>
-        <style>
-            html,body,iframe{width:100%;height:100%;margin:0}
-        </style>
-        """
-
-minhtml = replace(html, r"\s\s+|\n" => "") # hacky minifier
-
-datauri = "data:text/html,$(escapeuri(minhtml))"
-
-println(datauri)
+println(compute_api_url(data, path))
--- docs/js/start.js
+++ docs/js/start.js
@@ -1,34 +1,54 @@
-var fileSelector = document.getElementById('map-bin-selector');
-var authorSelector = document.getElementById('author-name');
-var mapNameSelector = document.getElementById('map-name');
-var download = () => {};
+/* global slugify URL CogBinEl CogBinElLoader CogDataWebSrc createDialog everestYaml JSZip saveAs fetch history */
+let fileSelector = document.getElementById('map-bin-selector');
+let authorSelector = document.getElementById('author-name');
+let mapNameSelector = document.getElementById('map-name');
+let blob = () => fileSelector.files[0];
+let name = () => fileSelector.value.split(/(\\|\/)/g).pop();
+(async () => {
+  if (window.location.hash.split(':').length == 2) {
+    let fileLabel = fileSelector.parentElement;
+    fileLabel.parentElement.removeChild(fileLabel);
+    let dataUri = `data:application/octet-stream;base64,${window.location.hash.split(':')[1]}`;
+    let hashBlob = await (await fetch(dataUri)).blob();
+    blob = () => hashBlob;
+    let hashName = window.location.hash.split(':')[0].replace(/^#/, '');
+    name = () => hashName;
+    history.replaceState("", document.title, window.location.pathname + window.location.search);
+  }
+})(); // async initialization
+let download = () => {};
 document.getElementById('form').addEventListener('submit', async e => {
   e.preventDefault();
-  var author = slugify(authorSelector.value, '_');
-  var uri = URL.createObjectURL(fileSelector.files[0]);
-  var data = new CogBinEl();
-  await CogBinElLoader.load(null, data, new CogDataWebSrc(), uri);
-  URL.revokeObjectURL(uri);
-  var mapName = mapNameSelector.value;
-  var levelsetSlug = slugify(mapName, '_');
-  var bin = '';
+  let author = slugify(authorSelector.value, '_');
+  let data = new CogBinEl();
+  let mapName = mapNameSelector.value;
+  let levelsetSlug = slugify(mapName, '_');
+  let uri = URL.createObjectURL(blob());
+  try {
+    await CogBinElLoader.load(null, data, new CogDataWebSrc(), uri);
+    URL.revokeObjectURL(uri);
+  } catch (e) {
+    console.error('[Postcard] Error: Invalid map!')
+    return;
+  }
+  let bin = '';
   try {
     bin = data.children.filter(e => e.name == 'meta')[0].attributes.Name + '.bin';
     console.log(`[Postcard] Read map name: ${bin}`);
   } catch (ex) {
-    bin = fileSelector.value.split(/(\\|\/)/g).pop();
+    bin = name();
   }
-  var dialog = createDialog({levelset: mapName, level: mapName, levelset_id: levelsetSlug, author, bin});
-  var yaml = everestYaml(levelsetSlug);
-  var zip = new JSZip();
+  let dialog = createDialog({levelset: mapName, level: mapName, levelset_id: levelsetSlug, author, bin});
+  let yaml = everestYaml(levelsetSlug);
+  let zip = new JSZip();
   zip.file('everest.yaml', yaml);
-  var dialogFolder = zip.folder('Dialog');
+  let dialogFolder = zip.folder('Dialog');
   dialogFolder.file('English.txt', dialog);
-  var mapsFolder = zip.folder('Maps');
-  var authorFolder = mapsFolder.folder(author);
-  var levelsetFolder = authorFolder.folder(levelsetSlug);
-  levelsetFolder.file(bin, fileSelector.files[0]);
-  var content = await zip.generateAsync({type: 'blob'});
+  let mapsFolder = zip.folder('Maps');
+  let authorFolder = mapsFolder.folder(author);
+  let levelsetFolder = authorFolder.folder(levelsetSlug);
+  levelsetFolder.file(bin, blob());
+  let content = await zip.generateAsync({type: 'blob'});
   download = () => saveAs(content, `${levelsetSlug}.zip`);
   document.body.className += ' done';
 });
